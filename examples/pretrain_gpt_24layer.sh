81468a4e9491cd03f4cd8c884688375db4c93ea1
Jeffrey Huynh <jthuynh@amazon.com>
2022-07-21 19:32:10 +0000

    Add scaffolding in test scripts to help with compilation and testing

--- examples/pretrain_gpt_24layer.sh
+++ examples/pretrain_gpt_24layer.sh
@@ -1,5 +1,7 @@
+#! /bin/bash
+set -o pipefail
+
 DATA_PATH=~/examples_datasets/gpt2/my-gpt2_text_document
-CHECKPOINT_PATH=~/examples
 
 export MASTER_ADDR='localhost'
 export MASTER_PORT=6000
@@ -15,6 +17,11 @@ export TF_GRPC_WORKER_CACHE_THREADS=4
 
 export NEURON_INTERNAL_TRANSFER_ALL_PARAMETERS_WITH_STATIC_RING=1
 
+TRAIN_ITERS=10000
+if [[ "$NEURON_EXTRACT_GRAPHS_ONLY" == "1" ]]; then
+    TRAIN_ITERS=68
+fi
+
 python3 pretrain_gpt_mp.py \
     --tensor-model-parallel-size 32 \
     --num-layers 24 \
@@ -24,7 +31,7 @@ python3 pretrain_gpt_mp.py \
     --global-batch-size 64 \
     --seq-length 2048 \
     --max-position-embeddings 2048 \
-    --train-iters 10000 \
+    --train-iters $TRAIN_ITERS \
     --lr-decay-iters 320000 \
     --data-path $DATA_PATH \
     --vocab-file ~/examples_datasets/gpt2/gpt2-vocab.json \
@@ -48,5 +55,21 @@ python3 pretrain_gpt_mp.py \
     --no-bias-dropout-fusion \
     --no-async-tensor-model-parallel-allreduce \
     --no-contiguous-buffers-in-local-ddp \
-    --tensorboard-dir ./tb_gpt2_32layer_fp32 \
-    |& tee run_log_gpt2_32layer_fp32
+    --tensorboard-dir ./tb_gpt2_24layer_fp32 \
+    |& tee run_log_gpt2_24layer_fp32
+
+ret_val=$?
+if [ $ret_val -eq 0 ]; then
+    success=1
+else
+    success=0
+fi
+
+dump_to_s3_update_json_scr=../../dump_to_s3_update_test_json.sh
+if [ -e $dump_to_s3_update_json_scr ]; then
+    $dump_to_s3_update_json_scr $@ --key=inference_success --value=$success || echo "Unable to update test result JSON."
+else
+    echo "WARNING: Script $dump_to_s3_update_json_scr not found. Not updating test result JSON."
+fi
+
+exit $ret_val
--- examples/pretrain_gpt_32layer.sh
+++ examples/pretrain_gpt_32layer.sh
@@ -1,3 +1,6 @@
+#! /bin/bash
+set -o pipefail
+
 DATA_PATH=~/examples_datasets/gpt2/my-gpt2_text_document
 CHECKPOINT_PATH=~/examples
 
@@ -13,7 +16,14 @@ export XLA_THREAD_POOL_SIZE=2
 export TF_GRPC_WORKER_CACHE_QUEUES=4
 export TF_GRPC_WORKER_CACHE_THREADS=4
 
+export NEURON_NUM_RECENT_MODELS_TO_KEEP=3
+export NEURON_RT_ONE_TMPBUF_PAGE_SIZE_MB=1024
 export NEURON_INTERNAL_TRANSFER_ALL_PARAMETERS_WITH_STATIC_RING=1
+
+TRAIN_ITERS=10000
+if [[ "$NEURON_EXTRACT_GRAPHS_ONLY" == "1" ]]; then
+    TRAIN_ITERS=68
+fi
 
 python3 pretrain_gpt_mp.py \
     --tensor-model-parallel-size 32 \
@@ -24,7 +34,7 @@ python3 pretrain_gpt_mp.py \
     --global-batch-size 64 \
     --seq-length 2048 \
     --max-position-embeddings 2048 \
-    --train-iters 10000 \
+    --train-iters $TRAIN_ITERS \
     --lr-decay-iters 320000 \
     --data-path $DATA_PATH \
     --vocab-file ~/examples_datasets/gpt2/gpt2-vocab.json \
@@ -50,3 +60,19 @@ python3 pretrain_gpt_mp.py \
     --no-contiguous-buffers-in-local-ddp \
     --tensorboard-dir ./tb_gpt2_32layer_fp32 \
     |& tee run_log_gpt2_32layer_fp32
+
+ret_val=$?
+if [ $ret_val -eq 0 ]; then
+    success=1
+else
+    success=0
+fi
+
+dump_to_s3_update_json_scr=../../dump_to_s3_update_test_json.sh
+if [ -e $dump_to_s3_update_json_scr ]; then
+    $dump_to_s3_update_json_scr $@ --key=inference_success --value=$success || echo "Unable to update test result JSON."
+else
+    echo "WARNING: Script $dump_to_s3_update_json_scr not found. Not updating test result JSON."
+fi
+
+exit $ret_val
